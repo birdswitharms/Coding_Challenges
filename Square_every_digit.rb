5a66d2bc5669bcdf2dec2371f9478a8f1e101fea
ANDREW SEGAL <segal.a@hotmail.com>
2018-03-27 17:05:09 -0400

    completed

--- Square_every_digit.rb
+++ Square_every_digit.rb
@@ -3,3 +3,18 @@
 # For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
 #
 # Note: The function accepts an integer and returns an integer
+
+# My answer:
+
+def square_digits(num)
+  num.to_s.split("").map{ |n| n.to_i * n.to_i }.join.to_i
+end
+
+puts square_digits(9119)
+puts square_digits(91198182)
+
+# Best answer from Codewars:
+
+# def square_digits num
+#   num.to_s.chars.map{|x| x.to_i**2}.join.to_i
+# end
