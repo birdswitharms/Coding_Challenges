7dd1053c14ae90050c6a8db8a123bff1ae4fa56d
ANDREW SEGAL <segal.a@hotmail.com>
2018-03-27 21:18:44 -0400

    completed

--- Isograms.rb
+++ Isograms.rb
@@ -3,3 +3,17 @@
 # is_isogram("Dermatoglyphics" ) == true
 # is_isogram("aba" ) == false
 # is_isogram("moOse" ) == false # -- ignore letter case
+
+# My answer:
+
+def is_isogram(string)
+  string.downcase.chars.uniq == string.downcase.chars
+end
+
+
+# Best answer from Codewars:
+def is_isogram(string)
+  string.downcase.chars.uniq == string.downcase.chars
+end
+
+# first time I've got it exactly the same!!
