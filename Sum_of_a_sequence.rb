6319de332ff225461b2995fff1b5071ae8fa87da
ANDREW SEGAL <segal.a@hotmail.com>
2018-05-16 14:24:30 -0400

    completed

--- Sum_of_a_sequence.rb
+++ Sum_of_a_sequence.rb
@@ -14,10 +14,16 @@
 def sequence_sum(begin_number, end_number, step)
   result = 0
   return result if begin_number > end_number
-  (end_number / step.to_f).ceil.times do |n|
+  return end_number if begin_number == end_number
+  steps_taken = (end_number / step.to_f).ceil
+  steps_taken.times do |n|
     result += (begin_number)+(step*n)
   end
+  if begin_number == 0
+    result += end_number
+  end  
   return result
 end
 
-puts sequence_sum(6,5,1)
+puts sequence_sum(0,15,3) #expected 45
+# 0 + 3 + 6 + 9 + 12 + 15
